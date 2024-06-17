879a48b43a3adc5607b34d243b0f300066145277
chloeeyline <chloe.eyline.hauer@gmail.com>
2024-06-17 11:59:13 +0200

    Adjusted Code seperation to better match the MVC Principle

--- Mandelbrot/src/main/java/com/mandelbrot/display/DisplayController.java
+++ Mandelbrot/src/main/java/com/mandelbrot/display/DisplayController.java
@@ -2,69 +2,84 @@ package com.mandelbrot.display;
 
 import com.mandelbrot.base.BaseController;
 import javafx.scene.image.PixelWriter;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class DisplayController extends BaseController<DisplayView, DisplayModel> {
     public DisplayController(DisplayModel model, DisplayView view) {
-        super(model,view);
-        _view.drawMandelbrotSet();
+        super(model, view);
+        drawMandelbrotSet();
     }
 
-    public void drawMandelbrotSet(PixelWriter pw) {
+    public void drawMandelbrotSet() {
+        PixelWriter pw = getView().getImage().getPixelWriter();
+
         for (int x = 0; x < getModel().getViewDataModel().getImageWidth(); x++) {
             for (int y = 0; y < getModel().getViewDataModel().getImageHeight(); y++) {
                 double zx = 0;
                 double zy = 0;
                 double cX = getModel().getDataModel().getXMin() + (x / getModel().getViewDataModel().getImageWidth()) * (getModel().getDataModel().getXMax() - getModel().getDataModel().getXMin());
                 double cY = getModel().getDataModel().getYMin() + (y / getModel().getViewDataModel().getImageHeight()) * (getModel().getDataModel().getYMax() - getModel().getDataModel().getYMin());
-                int iter = _model.computeIterations(zx, zy, cX, cY);
-                pw.setColor(x, y, _model.getColor(iter));
+                int iter = getModel().computeIterations(zx, zy, cX, cY);
+                pw.setColor(x, y, getModel().getColor(iter));
             }
+        }
+
+        // Ensure the image is drawn on the JavaFX Application Thread
+        javafx.application.Platform.runLater(() -> {
+            getView().getCanvas().getGraphicsContext2D().drawImage(getView().getImage(), 0, 0);
+        });
+    }
+
+    public void mouseReleased(MouseEvent evt) {
+        if (getModel().getViewDataModel().isDragging()) {
+            getView().getChildren().remove(getModel().getViewDataModel().getDragZoomRect());
+            getModel().mouseReleased(evt.getX(), evt.getY());
+            drawMandelbrotSet();
         }
     }
 
-    public void doZoomInOnRect(Rectangle rect) {
-        double newXMin = getModel().getDataModel().getXMin() + (rect.getX() / getModel().getViewDataModel().getImageWidth()) * (getModel().getDataModel().getXMax() - getModel().getDataModel().getXMin());
-        double newXMax = getModel().getDataModel().getXMin() + ((rect.getX() + rect.getWidth()) / getModel().getViewDataModel().getImageWidth()) * (getModel().getDataModel().getXMax() - getModel().getDataModel().getXMin());
-        double newYMin = getModel().getDataModel().getYMin() + (rect.getY() / getModel().getViewDataModel().getImageHeight()) * (getModel().getDataModel().getYMax() - getModel().getDataModel().getYMin());
-        double newYMax = getModel().getDataModel().getYMin() + ((rect.getY() + rect.getHeight()) / getModel().getViewDataModel().getImageHeight()) * (getModel().getDataModel().getYMax() - getModel().getDataModel().getYMin());
-        getModel().getDataModel().setLimits(newXMin, newXMax, newYMin, newYMax);
-    }
+    public void mouseDragged(MouseEvent evt) {
+        if (!getModel().getViewDataModel().isDragging()) return;
+        double x = evt.getX();
+        double y = evt.getY();
 
-    public void doZoomOutFromRect(Rectangle rect) {
-        double rectX = rect.getX();
-        double rectY = rect.getY();
-        double rectW = rect.getWidth();
-        double rectH = rect.getHeight();
-        double imageWidth = getModel().getViewDataModel().getImageWidth();
-        double imageHeight = getModel().getViewDataModel().getImageHeight();
+        double offsetX = x - getModel().getViewDataModel().getStartX();
+        double offsetY = y - getModel().getViewDataModel().getStartY();
+        if (!getModel().getViewDataModel().isMovedMouse() && Math.abs(offsetX) < 3 && Math.abs(offsetY) < 3) return;
+        getModel().getViewDataModel().setMovedMouse(true);
 
-        double newPixelWidth = (getModel().getDataModel().getXMax() - getModel().getDataModel().getXMin()) / rectW;
-        double newPixelHeight = (getModel().getDataModel().getYMax() - getModel().getDataModel().getYMin()) / rectH;
+        if (getModel().getViewDataModel().getMouseAction() == DisplayViewDataModel.MOUSE_ACTION_DRAG) {
+            getModel().getViewDataModel().setDragOffsetX(offsetX);
+            getModel().getViewDataModel().setDragOffsetY(offsetY);
 
-        double newXMin = getModel().getDataModel().getXMin() - newPixelWidth * rectX;
-        double newYMax = getModel().getDataModel().getYMax() + newPixelHeight * rectY;
-        double newWidth = newPixelWidth * imageWidth;
-        double newHeight = newPixelHeight * imageHeight;
-        double newXMax = newXMin + newWidth;
-        double newYMin = newYMax - newHeight;
+            //clear the Canvas
+            getView().getCanvas().getGraphicsContext2D().setFill(Color.LIGHTGRAY);
+            getView().getCanvas().getGraphicsContext2D().fillRect(0, 0, getModel().getViewDataModel().getImageWidth(), getModel().getViewDataModel().getImageHeight());
 
-        getModel().getDataModel().setLimits(newXMin, newXMax, newYMin, newYMax);
-    }
-
-    public void doDrag(double startX, double startY, double endX, double endY) {
-        double xShift = endX - startX;
-        double yShift = endY - startY;
-
-        double xRange = getModel().getDataModel().getXMax() - getModel().getDataModel().getXMin();
-        double yRange = getModel().getDataModel().getYMax() - getModel().getDataModel().getYMin();
-
-        double newXMin = getModel().getDataModel().getXMin() - (xShift / getModel().getViewDataModel().getImageWidth()) * xRange;
-        double newXMax = getModel().getDataModel().getXMax() - (xShift / getModel().getViewDataModel().getImageWidth()) * xRange;
-        double newYMin = getModel().getDataModel().getYMin() - (yShift / getModel().getViewDataModel().getImageHeight()) * yRange;
-        double newYMax = getModel().getDataModel().getYMax() - (yShift / getModel().getViewDataModel().getImageHeight()) * yRange;
-
-        getModel().getDataModel().setLimits(newXMin, newXMax, newYMin, newYMax);
-        _view.drawMandelbrotSet();
+            // Draw the shifted image
+            getView().getCanvas().getGraphicsContext2D().drawImage(getView().getImage(), getModel().getViewDataModel().getDragOffsetX(), getModel().getViewDataModel().getDragOffsetY());
+        } else if (getModel().getViewDataModel().getMouseAction() == DisplayViewDataModel.MOUSE_ACTION_ZOOM_IN ||
+                getModel().getViewDataModel().getMouseAction() == DisplayViewDataModel.MOUSE_ACTION_ZOOM_OUT) {
+            double width = Math.abs(offsetX);
+            double height = Math.abs(offsetY);
+            if (width < 3 || height < 3) {
+                getView().getChildren().remove(getModel().getViewDataModel().getDragZoomRect());
+                getModel().getViewDataModel().setDragZoomRect(null);
+            } else {
+                double aspect = getModel().getViewDataModel().getImageWidth() / getModel().getViewDataModel().getImageHeight();
+                double rectAspect = width / height;
+                if (aspect > rectAspect) width = width * aspect / rectAspect + 0.499;
+                else if (aspect < rectAspect) height = height * rectAspect / aspect + 0.499;
+                double xMin = getModel().getViewDataModel().getStartX() < x ? getModel().getViewDataModel().getStartX() : getModel().getViewDataModel().getStartX() - width;
+                double yMin = getModel().getViewDataModel().getStartY() < y ? getModel().getViewDataModel().getStartY() : getModel().getViewDataModel().getStartY() - height;
+                getView().getChildren().remove(getModel().getViewDataModel().getDragZoomRect());
+                getModel().getViewDataModel().setDragZoomRect(new Rectangle(xMin, yMin, width, height));
+                getModel().getViewDataModel().getDragZoomRect().setStroke(Color.BLACK);
+                getModel().getViewDataModel().getDragZoomRect().setFill(Color.TRANSPARENT);
+                getView().getChildren().add(getModel().getViewDataModel().getDragZoomRect());
+            }
+        }
     }
 }
--- Mandelbrot/src/main/java/com/mandelbrot/display/DisplayModel.java
+++ Mandelbrot/src/main/java/com/mandelbrot/display/DisplayModel.java
@@ -1,7 +1,9 @@
 package com.mandelbrot.display;
 
 import com.mandelbrot.base.BaseModel;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class DisplayModel extends BaseModel<DisplayDataModel, DisplayViewDataModel> {
     public DisplayModel() {
@@ -26,5 +28,94 @@ public class DisplayModel extends BaseModel<DisplayDataModel, DisplayViewDataMod
         }
         double hue = 360.0 * iter / getDataModel().getMaxIteration();
         return Color.hsb(hue, 1.0, iter / (iter + 8.0));
+    }
+
+    public void doDrag(double endX, double endY) {
+        double xShift = endX - getViewDataModel().getStartX();
+        double yShift = endY - getViewDataModel().getStartY();
+
+        double xRange = getDataModel().getXMax() - getDataModel().getXMin();
+        double yRange = getDataModel().getYMax() - getDataModel().getYMin();
+
+        double newXMin = getDataModel().getXMin() - (xShift / getViewDataModel().getImageWidth()) * xRange;
+        double newXMax = getDataModel().getXMax() - (xShift / getViewDataModel().getImageWidth()) * xRange;
+        double newYMin = getDataModel().getYMin() - (yShift / getViewDataModel().getImageHeight()) * yRange;
+        double newYMax = getDataModel().getYMax() - (yShift / getViewDataModel().getImageHeight()) * yRange;
+
+        getDataModel().setLimits(newXMin, newXMax, newYMin, newYMax);
+    }
+    
+    public void doZoomInOnRect() {
+        double newXMin = getDataModel().getXMin() + (getViewDataModel().getDragZoomRect().getX() / getViewDataModel().getImageWidth()) * (getDataModel().getXMax() - getDataModel().getXMin());
+        double newXMax = getDataModel().getXMin() + ((getViewDataModel().getDragZoomRect().getX() + getViewDataModel().getDragZoomRect().getWidth()) / getViewDataModel().getImageWidth()) * (getDataModel().getXMax() - getDataModel().getXMin());
+        double newYMin = getDataModel().getYMin() + (getViewDataModel().getDragZoomRect().getY() / getViewDataModel().getImageHeight()) * (getDataModel().getYMax() - getDataModel().getYMin());
+        double newYMax = getDataModel().getYMin() + ((getViewDataModel().getDragZoomRect().getY() + getViewDataModel().getDragZoomRect().getHeight()) / getViewDataModel().getImageHeight()) * (getDataModel().getYMax() - getDataModel().getYMin());
+        getDataModel().setLimits(newXMin, newXMax, newYMin, newYMax);
+    }
+    
+    public void doZoomOutFromRect() {
+        if(getViewDataModel().getDragZoomRect() == null) return;
+        double rectX = getViewDataModel().getDragZoomRect().getX();
+        double rectY = getViewDataModel().getDragZoomRect().getY();
+        double rectW = getViewDataModel().getDragZoomRect().getWidth();
+        double rectH = getViewDataModel().getDragZoomRect().getHeight();
+        double imageWidth = getViewDataModel().getImageWidth();
+        double imageHeight = getViewDataModel().getImageHeight();
+
+        double newPixelWidth = (getDataModel().getXMax() - getDataModel().getXMin()) / rectW;
+        double newPixelHeight = (getDataModel().getYMax() - getDataModel().getYMin()) / rectH;
+
+        double newXMin = getDataModel().getXMin() - newPixelWidth * rectX;
+        double newYMax = getDataModel().getYMax() + newPixelHeight * rectY;
+        double newWidth = newPixelWidth * imageWidth;
+        double newHeight = newPixelHeight * imageHeight;
+        double newXMax = newXMin + newWidth;
+        double newYMin = newYMax - newHeight;
+
+        getDataModel().setLimits(newXMin, newXMax, newYMin, newYMax);
+    }
+
+    public void mousePressed(MouseEvent evt) {
+        if (!getViewDataModel().isDragging()) {
+            getViewDataModel().setStartX(evt.getX());
+            getViewDataModel().setStartY(evt.getY());
+
+            if (getViewDataModel().getStartX() > getViewDataModel().getImageWidth() ||
+                    getViewDataModel().getStartY() > getViewDataModel().getImageHeight()) return;
+
+            if (evt.isPrimaryButtonDown())
+                getViewDataModel().setMouseAction(DisplayViewDataModel.MOUSE_ACTION_DRAG);
+            else if (evt.isSecondaryButtonDown())
+                getViewDataModel().setMouseAction(DisplayViewDataModel.MOUSE_ACTION_ZOOM_IN);
+            else if (evt.isMiddleButtonDown())
+                getViewDataModel().setMouseAction(DisplayViewDataModel.MOUSE_ACTION_ZOOM_OUT);
+            else return;
+
+            getViewDataModel().setDragging(true);
+            getViewDataModel().setMovedMouse(false);
+        }
+
+    }
+
+    public void mouseReleased(double x, double y) {
+        if (!getViewDataModel().isDragging()) return;
+
+        if (getViewDataModel().getMouseAction() == DisplayViewDataModel.MOUSE_ACTION_DRAG &&
+                (getViewDataModel().getDragOffsetX() != 0 ||
+                        getViewDataModel().getDragOffsetY() != 0)) {
+            doDrag(x, y);
+        } else if (getViewDataModel().getMouseAction() == DisplayViewDataModel.MOUSE_ACTION_ZOOM_IN &&
+                getViewDataModel().getDragZoomRect() != null) {
+            doZoomInOnRect();
+        } else if (getViewDataModel().getMouseAction() == DisplayViewDataModel.MOUSE_ACTION_ZOOM_OUT) {
+            doZoomOutFromRect();
+        }
+
+        if (getViewDataModel().isDragging()) {
+            getViewDataModel().setDragging(false);
+            getViewDataModel().setDragZoomRect(null);
+            getViewDataModel().setDragOffsetX(0);
+            getViewDataModel().setDragOffsetX(1);
+        }
     }
 }
